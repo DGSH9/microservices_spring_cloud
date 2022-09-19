17f31de960fe4a29c711a4db9eaf29ae3b472d2d
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-19 13:11:30 +0530

    06 - Connect Spring Cloud Config Server to Local Git Repository

---
+++ spring-cloud-config-server/src/test/java/com/dgsh/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsh.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
