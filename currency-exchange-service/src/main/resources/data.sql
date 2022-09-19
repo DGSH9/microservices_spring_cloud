d7fd5f67d51f7bdff412da010622b4be3b5069f2
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-20 00:04:57 +0530

    13 - JPA and H2 Database Connection_____Configure JPA and Initialized Data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+
+INSERT INTO CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','INR',97,'');
+
+INSERT INTO CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','INR',20,'');
