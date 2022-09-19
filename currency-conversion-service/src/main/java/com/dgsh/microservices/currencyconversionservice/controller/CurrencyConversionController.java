070309c35d3cf7f9056db8490140c9298e30972f
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-20 01:56:27 +0530

    18 - Feign to Call One Microservice to Other Microservice_____Using Feign REST Client for Service Invocation - V2

--- currency-conversion-service/src/main/java/com/dgsh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/dgsh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.dgsh.microservices.currencyconversionservice.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,30 +11,42 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.dgsh.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.dgsh.microservices.currencyconversionservice.feign.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
-	
-	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateTotalExchamgeAmount(
-			@PathVariable("from")String from,
-			@PathVariable("to")String to,
-			@PathVariable("quantity") BigDecimal quantity) {
-		
-		//microservice call
+
+	@Autowired
+	private CurrencyExchangeProxy currencyExchangeProxy;
+
+	// Rest template
+	@GetMapping("/currency-conversion-rest/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateTotalExchamgeAmountUsingRestTemplate(@PathVariable("from") String from,
+			@PathVariable("to") String to, @PathVariable("quantity") BigDecimal quantity) {
+
+		// microservice call
 		HashMap<String, String> uriVariables = new HashMap<String, String>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
-		
+
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-				CurrencyConversion.class,uriVariables);
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
-		return new CurrencyConversion(currencyConversion.getId(),from,to,quantity,
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
 				currencyConversion.getConversionMultiple(),
-				quantity.multiply(currencyConversion.getConversionMultiple()),
-				currencyConversion.getEnvironment());
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" "+"rest template");
 	}
-	
-	
+
+	// Using FEIGN
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateTotalExchamgeAmountUsingFeign(@PathVariable("from") String from,
+			@PathVariable("to") String to, @PathVariable("quantity") BigDecimal quantity) {
+
+		CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveAllExchange(from, to);
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" "+"feign");
+	}
+
 }
---
+++ currency-conversion-service/src/main/java/com/dgsh/microservices/currencyconversionservice/feign/CurrencyExchangeProxy.java
@@ -0,0 +1,18 @@
+package com.dgsh.microservices.currencyconversionservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.dgsh.microservices.currencyconversionservice.bean.CurrencyConversion;
+
+
+@FeignClient(name = "currency-exchange",url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveAllExchange(
+			@PathVariable("from") String from,
+			@PathVariable("to") String to);
+	
+}
